a66941003453a0bf0dc5f644827ca177c48d3c5d
Douglas Su <d0u9.su@outlook.com>
2022-05-16 17:29:27 +0800

    minor fixes

--- bpf/cilium_ebpf_basic/main.go
+++ bpf/cilium_ebpf_basic/main.go
@@ -1,63 +1,63 @@
 package main
 
 import (
-        "log"
+	"log"
 
-        "github.com/vishvananda/netlink"
-        "golang.org/x/sys/unix"
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go tc bpf/bpf.c -- -I./bpf
 
-const ETH_NAME  = "eth0"
+const ETH_NAME = "eth0"
 
 func main() {
-    var err error
+	var err error
 
-    objs := tcObjects{}
-    if err := loadTcObjects(&objs, nil); err != nil {
-        log.Fatalf("loading objects: %v", err)
-    }
-    defer objs.Close()
+	objs := tcObjects{}
+	if err := loadTcObjects(&objs, nil); err != nil {
+		log.Fatalf("loading objects: %v", err)
+	}
+	defer objs.Close()
 
-    progFd := objs.tcPrograms.TcMain.FD()
+	progFd := objs.tcPrograms.TcMain.FD()
 
-    eth0, err := netlink.LinkByName(ETH_NAME)
-    if err != nil {
-        log.Fatalf("cannot find %s: %v", ETH_NAME, err)
-    }
+	eth0, err := netlink.LinkByName(ETH_NAME)
+	if err != nil {
+		log.Fatalf("cannot find %s: %v", ETH_NAME, err)
+	}
 
-    attrs := netlink.QdiscAttrs{
-        LinkIndex: eth0.Attrs().Index,
-        Handle:    netlink.MakeHandle(0xffff, 0),
-        Parent:    netlink.HANDLE_CLSACT,
-    }
+	attrs := netlink.QdiscAttrs{
+		LinkIndex: eth0.Attrs().Index,
+		Handle:    netlink.MakeHandle(0xffff, 0),
+		Parent:    netlink.HANDLE_CLSACT,
+	}
 
-    qdisc := &netlink.GenericQdisc{
-        QdiscAttrs: attrs,
-        QdiscType:  "clsact",
-    }
+	qdisc := &netlink.GenericQdisc{
+		QdiscAttrs: attrs,
+		QdiscType:  "clsact",
+	}
 
-    if err := netlink.QdiscAdd(qdisc); err != nil {
-        log.Fatalf("cannot add clsact qdisc: %v", err)
-    }
+	if err := netlink.QdiscAdd(qdisc); err != nil {
+		log.Fatalf("cannot add clsact qdisc: %v", err)
+	}
 
-    filterAttrs := netlink.FilterAttrs{
-        LinkIndex: eth0.Attrs().Index,
-        Parent:    netlink.HANDLE_MIN_INGRESS,
-        Handle:    netlink.MakeHandle(0, 1),
-        Protocol:  unix.ETH_P_ALL,
-        Priority:  1,
-    }
+	filterAttrs := netlink.FilterAttrs{
+		LinkIndex: eth0.Attrs().Index,
+		Parent:    netlink.HANDLE_MIN_INGRESS,
+		Handle:    netlink.MakeHandle(0, 1),
+		Protocol:  unix.ETH_P_ALL,
+		Priority:  1,
+	}
 
-    filter := &netlink.BpfFilter{
-        FilterAttrs:  filterAttrs,
-        Fd:           progFd,
-        Name:         "hi-tc",
-        DirectAction: true,
-    }
+	filter := &netlink.BpfFilter{
+		FilterAttrs:  filterAttrs,
+		Fd:           progFd,
+		Name:         "hi-tc",
+		DirectAction: true,
+	}
 
-    if err := netlink.FilterAdd(filter); err != nil {
-        log.Fatalf("cannot attach bpf object to filter: %v", err);
-    }
+	if err := netlink.FilterAdd(filter); err != nil {
+		log.Fatalf("cannot attach bpf object to filter: %v", err)
+	}
 }
--- bpf/cilium_ebpf_pinned_map/replace_with_another_map/main.go
+++ bpf/cilium_ebpf_pinned_map/replace_with_another_map/main.go
@@ -3,73 +3,82 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"path"
+
 	"github.com/cilium/ebpf"
 )
 
 //go:generate go run github.com/cilium/ebpf/cmd/bpf2go prog bpf/bpf.c -- -I../include
 
 const ETH_NAME = "eth0"
-const BPF_FS    = "/sys/fs/bpf"
+const BPF_FS = "/sys/fs/bpf"
 
 type Map3Spec struct {
-    inner   *ebpf.MapSpec  `ebpf:"map_3"`
+	inner *ebpf.MapSpec `ebpf:"map_3"`
 }
 
 func LoadMap3FromFile(specs *ebpf.CollectionSpec) *ebpf.Map {
-	opt := ebpf.MapOptions{
-			PinPath: path.Join(BPF_FS, "map3_dir"),
+	dir := path.Join(BPF_FS, "map3_dir")
+	if err := os.MkdirAll(dir, 0644); err != nil {
+		log.Fatalf("cannot mkdir %s", dir)
 	}
-    mp, err := ebpf.NewMapWithOptions(specs.Maps["map_3"], opt);
-    if err != nil {
-        log.Fatalf("cannot load map3: %v", err)
-    }
-    return mp
+	opt := ebpf.MapOptions{
+		PinPath: dir,
+	}
+	mp, err := ebpf.NewMapWithOptions(specs.Maps["map_3"], opt)
+	if err != nil {
+		log.Fatalf("cannot load map3: %v", err)
+	}
+	return mp
 }
 
 func LoadMap4FromFile(specs *ebpf.CollectionSpec) *ebpf.Map {
-	opt := ebpf.MapOptions{
-			PinPath: path.Join(BPF_FS, "map4_dir"),
+	dir := path.Join(BPF_FS, "map4_dir")
+	if err := os.MkdirAll(dir, 0644); err != nil {
+		log.Fatalf("cannot mkdir %s", dir)
 	}
-    mp, err := ebpf.NewMapWithOptions(specs.Maps["map_4"], opt);
-    if err != nil {
-        log.Fatalf("cannot load map4: %v", err)
-    }
-    return mp
+	opt := ebpf.MapOptions{
+		PinPath: dir,
+	}
+	mp, err := ebpf.NewMapWithOptions(specs.Maps["map_4"], opt)
+	if err != nil {
+		log.Fatalf("cannot load map4: %v", err)
+	}
+	return mp
 }
 
 func main() {
-    specs, err := loadProg()
-    if err != nil {
-        log.Fatalf("load prog spec failed: %v", err)
-    }
+	specs, err := loadProg()
+	if err != nil {
+		log.Fatalf("load prog spec failed: %v", err)
+	}
 
-    map3 := LoadMap3FromFile(specs)
-    map4 := LoadMap4FromFile(specs)
+	map3 := LoadMap3FromFile(specs)
+	map4 := LoadMap4FromFile(specs)
 
-    fmt.Println(map3, map4)
-    rewriteMap := map[string]*ebpf.Map {
-        "map_3": map3,
-        "map_4": map4,
-    }
+	fmt.Println(map3, map4)
+	rewriteMap := map[string]*ebpf.Map{
+		"map_3": map3,
+		"map_4": map4,
+	}
 
-    fmt.Printf("%v\n", specs);
-    if err := specs.RewriteMaps(rewriteMap); err != nil {
-        log.Fatalf("rewrite map failed: %v\n", err)
-    }
+	fmt.Printf("%v\n", specs)
+	if err := specs.RewriteMaps(rewriteMap); err != nil {
+		log.Fatalf("rewrite map failed: %v\n", err)
+	}
 
-    fmt.Printf("%v\n", specs);
+	fmt.Printf("%v\n", specs)
 
-
-    objs := progPrograms{}
-    opts := ebpf.CollectionOptions {
-        Maps: ebpf.MapOptions {
+	objs := progPrograms{}
+	opts := ebpf.CollectionOptions{
+		Maps: ebpf.MapOptions{
 			PinPath: BPF_FS,
-        },
-    }
-    if err := specs.LoadAndAssign(&objs, &opts); err != nil {
-        log.Fatalf("LoadAndAssign Failed: %v\n", err)
-    }
+		},
+	}
+	if err := specs.LoadAndAssign(&objs, &opts); err != nil {
+		log.Fatalf("LoadAndAssign Failed: %v\n", err)
+	}
 
 	var buffer string
 	fmt.Println("Press any key to continue")
